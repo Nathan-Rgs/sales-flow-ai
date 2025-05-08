a96e3c2dffd7eee0a2d9d9b27dfe2d34e876105d
LuanB-compt <luan-bruno-domingues@hotmail.com>
2025-05-08 19:54:34 -0300

    Created Factory Model for model connecation

---
+++ src/classify/model.py
@@ -0,0 +1,38 @@
+from decouple import config
+from langchain_core.language_models.chat_models import BaseChatModel
+from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
+from interface.model import ModelInterface
+
+class ClassifyModelFactory(ModelInterface):
+
+    @classmethod
+    def connect_factory(cls, temperature: float) -> BaseChatModel:
+        return ClassifyModelFactory.__connect_cloud_gpt(temperature=temperature)
+
+    @classmethod
+    def __connect_local_ollama(cls, temperature: float) -> ChatOllama:
+        try:
+            return ChatOllama(
+                model=config('ollama_model_name'),
+                base_url=config('local_url_ollama'),
+                temperature=temperature,
+                streaming=False
+            )
+        except Exception as e:
+            raise e
+
+    @classmethod
+    def __connect_cloud_gpt(cls, temperature: float) -> BaseChatModel:
+        try:
+            return ChatOpenAI(
+                model=config("gpt_model_name"),
+                temperature=temperature,
+                max_tokens=None,
+                timeout=None,
+                max_retries=2,
+                api_key=config('gpt_model_api_key'),
+                streaming=False
+            )
+        except Exception as e:
+            raise e
