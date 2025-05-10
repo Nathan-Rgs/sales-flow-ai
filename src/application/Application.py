287f0da58784c4f9047912309dccecd1159d31c9
LuanB-compt <luan-bruno-domingues@hotmail.com>
2025-05-10 17:00:11 -0300

    Implemented info controller in application RAG

--- src/application/Application.py
+++ src/application/Application.py
@@ -1,32 +1,31 @@
 from classify.controller import ClassifyController
+from info.controller import InfoController
 from decouple import config
 from typing import List
 
 class ApplicationRAG():
-    __responser_classify: ClassifyController
+    __controller_classify: ClassifyController
+    __controller_info: InfoController
     __tags = List[str]
 
     def __init__(self):
-        self.__responser_classify = ClassifyController()
+        self.__controller_classify = ClassifyController()
+        self.__controller_info = InfoController()
         self.__tags = self.__get_tags()
-        print(self.__tags)
 
     async def run(self, input: str) -> str:
         if not self.__is_valid_input(input=input):
             raise Exception("Not Valid Input")
-        tag = await self.__responser_classify.get_response(input=input)
+        tag = await self.__controller_classify.get_response(input=input)
         if tag == self.__tags[0]:
             ...
         elif tag == self.__tags[1]:
-            ...
+            return await self.__controller_info.get_response(input=input)
         elif tag == self.__tags[2]:
             ...
         else:
             ...
-        return tag
-
-    def __get_response() -> str:
-        ...
+        return 'erro'
 
     def __is_valid_input(self, input: str) -> bool:
         if len(input) < 1:
@@ -34,5 +33,5 @@ class ApplicationRAG():
         return True
 
     def __get_tags(self) -> List[str]:
-        tags: str = config('tags')
+        tags: str = config('TAGS')
         return tags.lower().split(sep=', ')
