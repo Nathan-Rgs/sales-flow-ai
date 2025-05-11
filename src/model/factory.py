492b1a142d19bef59cd77fc1f2672c9020206dcc
LuanB-compt <luan-bruno-domingues@hotmail.com>
2025-05-11 13:13:03 -0300

    Implemented schema parameter for factoring LLM models

--- src/model/factory.py
+++ src/model/factory.py
@@ -3,29 +3,32 @@ from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
 from interface.model import ModelInterface
+from pydantic import BaseModel
 
 class ModelFactory(ModelInterface):
 
     @classmethod
-    def connect_factory(cls, temperature: float) -> BaseChatModel:
-        return ModelFactory.__connect_cloud_gpt(temperature=temperature)
+    def connect_factory(cls, temperature: float, schema: BaseModel | None = None) -> BaseChatModel:
+        return ModelFactory.__connect_cloud_gpt(temperature=temperature, schema=schema)
 
     @classmethod
-    def __connect_local_ollama(cls, temperature: float) -> ChatOllama:
+    def __connect_local_ollama(cls, temperature: float, schema: BaseModel | None = None) -> ChatOllama:
         try:
-            return ChatOllama(
+            llm = ChatOllama(
                 model=config(''),
                 base_url=config(''),
                 temperature=temperature,
                 streaming=False
             )
+            if schema == None: return llm
+            return llm.with_structured_output(schema=schema)
         except Exception as e:
             raise e
 
     @classmethod
-    def __connect_cloud_gpt(cls, temperature: float) -> BaseChatModel:
+    def __connect_cloud_gpt(cls, temperature: float, schema: BaseModel | None = None) -> BaseChatModel:
         try:
-            return ChatOpenAI(
+            llm = ChatOpenAI(
                 model=config("GPT_MODEL_NAME"),
                 temperature=temperature,
                 max_tokens=None,
@@ -34,5 +37,7 @@ class ModelFactory(ModelInterface):
                 api_key=config('GPT_MODEL_API_KEY'),
                 streaming=False
             )
+            if schema == None: return llm
+            return llm.with_structured_output(schema=schema)
         except Exception as e:
             raise e
