39ae8e19d1233b6632080fea3cd89cb613bc9ba8
LuanB-compt <luan-bruno-domingues@hotmail.com>
2025-05-12 01:18:16 -0300

    Created database layer

--- src/price/controller.py
+++ src/price/controller.py
@@ -2,7 +2,7 @@ from common import get_prompt_from_file
 from decouple import config
 from model.factory import ModelFactory
 from price.prompt import PricePrompterFactory
-from price.database import PriceDatabase
+from database.database import Database
 from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_core.runnables import Runnable, RunnableWithMessageHistory
 from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
@@ -10,13 +10,13 @@ from memory.shared_memory import get_shared_history
 
 class PriceController():
 
-    __db: PriceDatabase
+    __db: Database
     __model: BaseChatModel
     __chain: Runnable
 
     def __init__(self):
         self.__model = ModelFactory().connect_factory(temperature=0.0)
-        self.__db = PriceDatabase()
+        self.__db = Database()
         self.__db.connect_database()
         self.__db.init_retreiver()
         self.__init_chain()
